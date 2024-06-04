c7f66103593b0c732caa39650690d41c00e6d0b3
Kaipo Kent <kaipo.kent@protonmail.me>
2024-06-04 13:27:13 -0600

    Add auto animate plugin for formkit

--- src/formkit.config.ts
+++ src/formkit.config.ts
@@ -1,8 +1,10 @@
 import { defaultConfig } from '@formkit/vue'
 import { rootClasses } from '@/formkit.theme.ts'
+import { createAutoAnimatePlugin } from '@formkit/addons'
 
 export default defaultConfig({
   config: {
     rootClasses
-  }
+  },
+  plugins: [createAutoAnimatePlugin()]
 })
--- src/views/ThreadView.vue
+++ src/views/ThreadView.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
+import { computed, ref } from 'vue'
 import { threads } from '../data.json'
 import { useRoute } from 'vue-router'
 import PostList from '@/components/PostList.vue'
-import { computed } from 'vue'
+import Button from 'primevue/button'
+import { reset } from '@formkit/vue'
+
 const route = useRoute()
 
+const formData = ref({ postBody: '' })
+
 const thread = computed(() => threads[route.params.id])
+const addPost = async (data: {}) => {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+  reset('addPost')
+}
 </script>
 
 <template>
@@ -13,5 +22,25 @@ const thread = computed(() => threads[route.params.id])
     <h1>{{ thread.title }}</h1>
 
     <PostList :threadPosts="thread.posts" />
+
+    <FormKit name="addPost" type="form" @submit="addPost" v-model="formData">
+      <FormKit
+        type="textarea"
+        name="postBody"
+        label="Add a post"
+        input-class="w-full"
+        :help="`${formData.postBody.length} / 120`"
+        validation="length:0,120"
+        validation-visibility="live"
+        :validation-messages="{
+          length: 'Post cannot be more than 120 characters.'
+        }"
+        rows="5"
+        cols="30"
+      />
+      <template #submit="{ state }">
+        <Button type="submit" :disabled="state.loading" label="Add post" />
+      </template>
+    </FormKit>
   </div>
 </template>
